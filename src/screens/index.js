e62bdd7b3fd74b40499172c6531cb0751ba236d4
Julius-745 <54503854+Julius-745@users.noreply.github.com>
2022-06-22 12:34:31 +0700

    fix

--- src/screens/index.js
+++ src/screens/index.js
@@ -8,11 +8,8 @@ import Home from './Home';
 import Jual from './Jual';
 import Notifikasi from './Notifikasi';
 import Splash from './Splash';
-<<<<<<< HEAD
 import InfoPenawar from './InfoPenawar';
-=======
 import DetailProduk from './DetailProduk';
->>>>>>> f40331a078731cb6c422a4c9d8bfed42f3c7e346
 export {
   Login,
   Profile,
@@ -24,9 +21,6 @@ export {
   Notifikasi,
   Splash,
   TambahBarang,
-<<<<<<< HEAD
   InfoPenawar,
-=======
   DetailProduk,
->>>>>>> f40331a078731cb6c422a4c9d8bfed42f3c7e346
 };
